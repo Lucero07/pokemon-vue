61e6a3ec7d143c637102aae505c5a8af925ca520
Lucero07 <ing.lucero.ramos@gmail.com>
2020-04-27 03:44:04 -0500

    delete semicolon

--- src/components/CardPokemon.vue
+++ src/components/CardPokemon.vue
@@ -2,14 +2,14 @@
   <v-card class="mx-auto" max-width="344" outlined>
     <v-list-item three-line>
       <v-list-item-content>
-        <v-list-item-title class="headline mb-3 text-uppercase" >{{ pokemon.name }}</v-list-item-title>
-        <v-list-item-subtitle>Alto: {{ pokemon.height }} cm</v-list-item-subtitle>
-        <v-list-item-subtitle>Peso: {{pokemon.weight}} Kg</v-list-item-subtitle>
-        <v-list-item-subtitle>Tipos: {{ getTypes(pokemon.types) }}</v-list-item-subtitle>
+        <v-list-item-title class="headline mb-3 text-uppercase" >{{ pokemon ? pokemon.name : 0 }}</v-list-item-title>
+        <v-list-item-subtitle>Alto: {{ pokemon ? pokemon.height : 0 }} cm</v-list-item-subtitle>
+        <v-list-item-subtitle>Peso: {{ pokemon ? pokemon.weight : 0}} Kg</v-list-item-subtitle>
+        <v-list-item-subtitle>Tipos: {{ pokemon.types ? getTypes(pokemon.types) : 'No tiene'  }}</v-list-item-subtitle>
       </v-list-item-content>
 
       <v-avatar size="82px">
-        <v-img :src="pokemon.sprites.front_default" alt="Vuetify" />
+        <v-img :src="pokemon.sprites ? pokemon.sprites.front_default : 'moche'" alt="Vuetify" />
       </v-avatar>
     </v-list-item>
 
@@ -38,10 +38,10 @@ export default {
   }),
   methods: {
     getTypes(types) {
-      const allTypes = [];
-      types.forEach(type => allTypes.push(type.type.name));
-      return allTypes.join(", ");
+      const allTypes = []
+      types.forEach(type => allTypes.push(type.type.name))
+      return allTypes.join(", ")
     }
   }
-};
+}
 </script>
